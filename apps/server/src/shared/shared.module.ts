2529e02f46a2b8b763b89a782031dbaa3120bdb9
Robin <lotosv2010@163.com>
2026-05-09 18:33:23 +0800

    Revert "fix(server): Redis 可选化，连接失败不阻塞启动"
    This reverts commit f6b41981a8b0e850bb740e6d8500c9b318b232ad.

--- apps/server/src/shared/shared.module.ts
+++ apps/server/src/shared/shared.module.ts
@@ -8,11 +8,10 @@ import { RedisModule } from "./redis/redis.module.js";
  * 全局基础设施模块
  *
  * 聚合 Logger + DatabaseModule + RedisModule + QueueModule + GeoIpService
- * QueueModule 异步注册：Redis 不可达时退化为空模块
  */
 @Global()
 @Module({
-  imports: [DatabaseModule, RedisModule, QueueModule.forRootAsync()],
+  imports: [DatabaseModule, RedisModule, QueueModule],
   providers: [Logger, GeoIpService],
   exports: [Logger, DatabaseModule, RedisModule, QueueModule, GeoIpService],
 })
